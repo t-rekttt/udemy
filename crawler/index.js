c128b81c4f83ad5a130e6b3ace5be8b73071b299
T-Rekt <t.to.the.rekt@gmail.com>
2018-03-09 11:06:48 +0700

    index.js

--- crawler/index.js
+++ crawler/index.js
@@ -8,5 +8,5 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
 	console.log('DB connected');
-	crawler.run();
+	crawler.run()
 });
